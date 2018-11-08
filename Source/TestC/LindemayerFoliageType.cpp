f96a335ff8b2091a28ddacd8fc449f38d52daea1
WenoZatteeuw <zenowatteeuw@hotmail.com>
2018-11-08 15:01:15 +0100

    Started FoliageTypeTesting
    Started testing for UFoliageType
    Made Dev Branch

---
+++ Source/TestC/LindemayerFoliageType.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LindemayerFoliageType.h"
+
+
+// Sets default values
+ULindemayerFoliageType::ULindemayerFoliageType()
+{
+ 	
+
+}
+
+
+
---
+++ Source/TestC/LindemayerFoliageType.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LindemayerFoliageType.generated.h"
+
+
+class UFoliageType;
+
+UCLASS()
+class TESTC_API ULindemayerFoliageType : public UObject
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ULindemayerFoliageType();
+
+protected:
+	
+
+public:	
+	UFoliageType* m_pFoliageType = nullptr;
+
+	
+	
+};
