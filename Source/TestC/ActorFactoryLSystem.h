59649e8cc18487eecb39a35c4a8cea5369ad6809
WenoZatteeuw <zenowatteeuw@hotmail.com>
2018-12-21 11:39:56 +0100

    Finished factories

---
+++ Source/TestC/ActorFactoryLSystem.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActorFactories/ActorFactoryBoxVolume.h"
+#include "ActorFactoryLSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTC_API UActorFactoryLSystem : public UActorFactoryBoxVolume
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/TestC/LSystemSpawnerFactory.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "LSystemSpawnerFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTC_API ULSystemSpawnerFactory : public UFactory
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
