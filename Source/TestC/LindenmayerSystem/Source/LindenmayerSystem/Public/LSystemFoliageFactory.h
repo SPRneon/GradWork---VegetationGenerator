0fb60e35328dd2ef1407723a24fd4472191e8a3c
WenoZatteeuw <zenowatteeuw@hotmail.com>
2018-12-21 16:49:59 +0100

    Added LSystemFoliageFactory

---
+++ Source/TestC/LindenmayerSystem/Source/LindenmayerSystem/Public/LSystemFoliageFactory.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ActorFactories/ActorFactory.h"
+#include "LSystemFoliageFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTC_API ULSystemFoliageFactory : public UActorFactory
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
